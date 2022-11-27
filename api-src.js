52d7e6f6d9f622859d1f7549f1ca033a3e7ec994
RemiNeal <57844355+RemiNeal@users.noreply.github.com>
2022-11-27 13:52:32 -0700

    Creative Project 2 is done

--- api-src.js
+++ api-src.js
@@ -2,6 +2,16 @@ function outputHTML(results){
     document.getElementById("imgs").innerHTML = results;
 }
 
+function outputKeywords(keywords){
+    let output = '<p>Keywords</p>';
+    output += '<ul id="keywords-list">';
+    for(const item of [...keywords]){
+        output += '<li class="keyword selectable">' + item + '</li>'
+    }
+    output += '</ul>';
+    document.getElementById("keywords").innerHTML = output;
+}
+
 document.getElementById("search-button").addEventListener("click", function(event) {
     event.preventDefault();
     const searchTerm = document.getElementById('search-bar').value;
@@ -20,22 +30,31 @@ document.getElementById("search-button").addEventListener("click", function(even
             let jsonItems = json.collection.items;
 
             let results = "";
+            let keywords = new Set();
             for(let i=0; i < jsonItems.length; i++){
                 let item = jsonItems[i];
                 results += '<div class="item">';
                 results += '<div class="img-title">' + item.data[0].title + '</div>';
                 results += '<img class="img-found" src="' + item.links[0].href + '" alt="Image not found"/>';
-                results += '<p class"img-description">' + item.data[0].description + '</p>';
 
+                results += '<div class="description">';
+                results += '<div>Desctiption</div>'; 
+                results += '<p class="img-description">' + item.data[0].description + '</p>';
+                results += '</div>';
+
+                results += '<div class="img-keywords">'
                 results += '<ul class="img-keywords">';
                 for(let j=0; j < item.data[0].keywords.length; j++){
-                    results += '<li class="keyword">' + item.data[0].keywords[j] + '</li>';
+                    results += '<li class="keyword selectable">' + item.data[0].keywords[j] + '</li>';
+                    keywords.add(item.data[0].keywords[j]);
                 }
                 results += '</ul>';
+                results += '</div>'
 
                 results += '</div>';
             }
             
+            outputKeywords(keywords);
             outputHTML(results);
         }).catch(function(){
             outputHTML(searchTerm + " was not found. Please try a different term.");
